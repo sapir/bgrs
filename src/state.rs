ac7efc824fff6c43303b74c3f10672dd33338a77
Y. Sapir <yasapir@gmail.com>
2018-09-12 15:14:15 +0300

    Add the "bar".

--- src/state.rs
+++ src/state.rs
@@ -28,24 +28,38 @@ impl PointState {
     }
 }
 
+type PointIndex = usize;
+
 #[derive(Clone, Debug)]
-pub struct BoardState([PointState; 24]);
+pub struct BoardState([PointState; 26]);
 
 impl BoardState {
+    fn get_opposite(index: PointIndex) -> PointIndex {
+        25 - index
+    }
+
     pub fn new() -> Self {
-        let mut points = [PointState::new(0, PlayerColor::Black); 24];
-        points[0] = PointState::new(2, PlayerColor::Black);
-        points[5] = PointState::new(5, PlayerColor::White);
-        points[7] = PointState::new(3, PlayerColor::White);
-        points[11] = PointState::new(5, PlayerColor::Black);
-        for i in 12..24 {
-            let opposite = points[23 - i];
+        let mut points = [PointState::new(0, PlayerColor::Black); 26];
+        points[1] = PointState::new(2, PlayerColor::Black);
+        points[6] = PointState::new(5, PlayerColor::White);
+        points[8] = PointState::new(3, PlayerColor::White);
+        points[12] = PointState::new(5, PlayerColor::Black);
+        for i in 13..26 {
+            // make a copy of point to work around borrow checker
+            let opposite = points[BoardState::get_opposite(i)];
             points[i] = PointState::new(
                 opposite.checker_count,
                 opposite.checker_color.inverse(),
             );
         }
         BoardState(points)
+    }
+
+    fn get_checker_string(color: PlayerColor) -> &'static str {
+        match color {
+            PlayerColor::Black => "b",
+            PlayerColor::White => "w",
+        }
     }
 
     fn print_points<'a, I>(&self, row_count: usize, points: I)
@@ -67,10 +81,7 @@ impl BoardState {
                     if row_count >= 5 {
                         "*"
                     } else {
-                        match point.checker_color {
-                            PlayerColor::Black => "b",
-                            PlayerColor::White => "w",
-                        }
+                        BoardState::get_checker_string(point.checker_color)
                     }
                 } else {
                     "|"
@@ -81,9 +92,20 @@ impl BoardState {
 
     pub fn print(&self) {
         for row in 0..6 {
-            self.print_points(row, (&self.0[12..18]).iter());
-            print!(" * ");
-            self.print_points(row, (&self.0[18..24]).iter());
+            self.print_points(row, (&self.0[13..19]).iter());
+
+            print!(
+                " {} ",
+                if row >= 6 - self.0[25].checker_count {
+                    BoardState::get_checker_string(
+                        self.0[25].checker_color,
+                    )
+                } else {
+                    "*"
+                }
+            );
+
+            self.print_points(row, (&self.0[19..25]).iter());
             println!();
         }
 
@@ -91,9 +113,18 @@ impl BoardState {
 
         for row in 0..6 {
             let row = 5 - row;
-            self.print_points(row, self.0[6..12].iter().rev());
-            print!(" * ");
-            self.print_points(row, self.0[0..6].iter().rev());
+            self.print_points(row, self.0[7..13].iter().rev());
+
+            print!(
+                " {} ",
+                if row >= 6 - self.0[0].checker_count {
+                    BoardState::get_checker_string(self.0[0].checker_color)
+                } else {
+                    "*"
+                }
+            );
+
+            self.print_points(row, self.0[1..7].iter().rev());
             println!();
         }
     }
