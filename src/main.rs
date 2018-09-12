edbec79035b79b4b41df232a1c7e608fe54d7c1c
Y. Sapir <yasapir@gmail.com>
2018-09-12 15:36:01 +0300

    Roll the dice.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+extern crate rand;
+
 mod state;
+
+use rand::Rng;
+
+fn roll_die() -> usize {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(1, 7)
+}
+
+fn roll_dice() -> (usize, usize) {
+    (roll_die(), roll_die())
+}
 
 fn main() {
     let board = state::BoardState::new();
     board.print();
+
+    let dice = roll_dice();
+    println!("Dice: {:?}", dice);
 }
